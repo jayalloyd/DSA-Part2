ca061bf95dc7b2786bc0d497164fc2a4057bdacc
jayalloyd <jayaranilloyd1983@gmail.com>
2024-11-21 22:16:24 +0530

    added find smallest number feature as well

--- FindLargestNumber.java
+++ FindLargestNumber.java
@@ -1,11 +1,16 @@
 public class FindLargestNumber {
     public static int findLarge(int numbers[]) {
-        int largest = Integer.MIN_VALUE; // Initialize to the smallest possible integer value
+        int smallest=Integer.MAX_VALUE;
+        int largest = Integer.MIN_VALUE; // Initialize to the smallest possible integer value -Infinity
         for (int i = 0; i < numbers.length; i++) { // Fix: Use i < numbers.length
             if (largest < numbers[i]) {
                 largest = numbers[i];
             }
+            if(smallest>numbers[i]){
+                smallest=numbers[i];
+            }
         }
+        System.out.println("The smallest number:"+smallest);
         return largest;
     }
 
