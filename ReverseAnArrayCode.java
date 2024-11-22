8afbee4a03feaadccc7f31ea921d568f66c3753e
jayalloyd <jayaranilloyd1983@gmail.com>
2024-11-22 06:26:04 +0530

    reverse an array added

---
+++ ReverseAnArrayCode.java
@@ -0,0 +1,23 @@
+public class ReverseAnArrayCode{
+    public static void reverseAnArray(int nums[]){
+      int start=0;
+        int end=nums.length-1;
+        while(start< end){
+
+            int temp=nums[start];
+            nums[start]=nums[end];
+            nums[end]=temp;
+            start++;
+            end--;
+
+    }}
+    public static void main(String[] args) {
+        int nums[]={3,4,7,8,10,11};
+        reverseAnArray(nums);
+        for(int i=0;i<nums.length;i++){
+            System.out.print(nums[i]+" ");
+
+        }
+        System.out.println();
+        }
+    }
