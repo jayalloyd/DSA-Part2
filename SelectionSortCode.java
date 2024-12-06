5bca39d75ae1e4db257807d63c13731851b569f3
jayalloyd <jayaranilloyd1983@gmail.com>
2024-12-06 14:03:15 +0530

    updated

--- SelectionSortCode.java
+++ SelectionSortCode.java
@@ -1,6 +1,6 @@
 public class SelectionSortCode {
     public static void selectionSort(int [] arr){
-       for(int i=0;i<arr.length;i++){//turns count
+       for(int i=0;i<arr.length-1;i++){//turns count
         int minPos=i;
         for(int j=i+1;j<arr.length;j++){
             if(arr[minPos]>arr[j]){
