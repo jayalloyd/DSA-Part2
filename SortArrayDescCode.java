5bb7e4932daa6d276738a1f3ff862611773d47f5
jayalloyd <jayaranilloyd1983@gmail.com>
2024-12-11 07:46:07 +0530

    sorting array using sorting techniques in desc

--- SortArrayDescCode.java
+++ SortArrayDescCode.java
@@ -17,6 +17,18 @@ public class SortArrayDescCode {
 //         }
 //         }
     //    
+    public static void selectionSort(int [] arr){
+       for(int i=0;i<arr.length-1;i++){//turns count
+        int minPos=i;
+        for(int j=i+1;j<arr.length;j++){
+            if(arr[minPos]<arr[j]){
+                minPos=j;
+            }
+        }
+       int temp=arr[minPos];
+       arr[minPos]=arr[i];
+       arr[i]=temp;
+    }}
     public static void insertionSort(int[] arr){
       for(int i=1;i<arr.length;i++){
             int curr=arr[i];
@@ -68,8 +80,8 @@ public class SortArrayDescCode {
     public static void main(String[] args) {
         int[] arr={3,6,2,1,8,7,4,5,3,1};
         // bubbleSort(arr);
-        // selectionSort(arr);
-        countingSort(arr);
+        selectionSort(arr);
+        // countingSort(arr);
         // insertionSort(arr);
         printArr(arr);
     }
