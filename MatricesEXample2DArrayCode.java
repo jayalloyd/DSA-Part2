0709949b0d7038c64e3931a88d5b08342ab0fc43
jayalloyd <jayaranilloyd1983@gmail.com>
2024-12-11 20:26:26 +0530

    search for a element key added

--- MatricesEXample2DArrayCode.java
+++ MatricesEXample2DArrayCode.java
@@ -1,9 +1,23 @@
 import java.util.Scanner;
 
 public class MatricesEXample2DArrayCode {
+    public static boolean search(int [][]matrix,int key){
+      for(int i=0;i<matrix.length;i++){
+        for (int j=0;j<matrix[0].length;j++){
+         if (matrix[i][j]==key){
+            System.out.print("Element found at:"+i +","+j);
+            return true;
+         }
+        }
+        System.out.println();
+     }
+         return false;
+     
+    }
+    
     public static void main(String[] args) {
     
-        Scanner in=new Scanner(System.in);
+     Scanner in=new Scanner(System.in);
      int [][] matrix=new int [3] [3];
            int n=matrix.length;
      int m=matrix[0].length;
@@ -18,7 +32,7 @@ public class MatricesEXample2DArrayCode {
         }
         System.out.println();
      }
-   
+      search(matrix, 2);
 
     }
 }
