9788914cf24a325dc75e5e7a1470916a241cf2b9
jayalloyd <jayaranilloyd1983@gmail.com>
2024-12-11 20:49:36 +0530

    largest and smallest number finding method added

--- MatricesEXample2DArrayCode.java
+++ MatricesEXample2DArrayCode.java
@@ -14,7 +14,43 @@ public class MatricesEXample2DArrayCode {
          return false;
      
     }
+
+
+    public static int smallestNumber(int[][] matrix){
+        int smallest=Integer.MAX_VALUE;
+        int smallestRow=-1;
+        int smallestCol=-1;
+        
+        for(int i=0;i<matrix.length;i++){
+        for (int j=0;j<matrix[0].length;j++){
+           int a= matrix[i][j];
+            if (a <smallest) {
+                smallest = a;
+           smallestRow = i;
+               smallestCol = j;
+            }
+            
+    }}System.out.print("Smallest Number is "+smallest +"Found at:" +smallestRow +","+smallestCol);
+            return smallest;
+}
     
+    public static int largestNumber(int [] [] matrix){
+        int largest=Integer.MIN_VALUE;    
+        int largestRow=-1;
+         int largestCol = -1;
+
+        for(int i=0;i<matrix.length;i++){
+        for (int j=0;j<matrix[0].length;j++){
+           int a= matrix[i][j];
+            if (a > largest) {
+                largest = a;
+                largestRow = i;
+                largestCol = j;
+            }
+            
+    }}System.out.print("Largest Number is "+largest +"Found at:" +largestRow +","+largestCol);
+            return largest;
+}
     public static void main(String[] args) {
     
      Scanner in=new Scanner(System.in);
@@ -23,16 +59,19 @@ public class MatricesEXample2DArrayCode {
      int m=matrix[0].length;
      for(int i=0;i<n;i++){
         for (int j=0;j<m;j++){
+
             matrix[i][j]=in.nextInt();
         }
      }
-      for(int i=0;i<n;i++){
+      for(int i=0;i<n;i++){ 
         for (int j=0;j<m;j++){
            System.out.print(matrix[i][j]+" ");
         }
         System.out.println();
      }
       search(matrix, 2);
-
+largestNumber(matrix); 
+smallestNumber(matrix);
+in.close();
     }
 }
