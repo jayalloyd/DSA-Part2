f416a80f5224179c4cb68d37c459f7430bebd8cc
Jaya Rani.Y.S <jayaranilloyd1983@gmail.com>
2025-01-26 23:34:31 +0530

    diagonal sum of matrix 2d added

---
+++ DiagonalSumDemo.java
@@ -0,0 +1,39 @@
+public class DiagonalSumDemo {
+
+    public static int diagonalMatrix(int matrix[] [])// O(n^2)
+    {int sum=0;
+        // for(int i=0; i< matrix.length ; i++)
+        // {
+            
+        //     for(int j=0;j<matrix[0].length;j++){
+        //         if(i==j){
+        //             sum+= matrix[i][j];
+
+        //         }
+        //         else if((i+j) == (matrix.length-1)){
+        //             sum += matrix[i][j];
+        //         }
+
+        //     }
+        // }
+        for(int i=0; i < matrix.length ; i++){
+            sum += matrix[i][i];//primary diagonal
+
+            //secondary diagonal
+
+            if(i != matrix.length-1-i)//i not equal to j so that middle element wont get added once more middle element is common for n=odd number
+             sum+= matrix[i][matrix.length-1-i];
+        }
+        return sum;
+        }
+        
+    
+    public static void main(String[] args) {
+        int matrix [] [] ={{1,2,3,4},
+                            {5,6,7,8},
+                            {9,10,11,12},
+                            {13,14,15,16}};
+     System.out.println(diagonalMatrix(matrix));               
+
+    }
+}
